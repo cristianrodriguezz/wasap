9630ae28d6be3f12c6f0853c149c3c2ffc8d6fe7
Cristian Rodriguez <cristiandosespigas@gmail.com>
2023-09-20 16:18:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useRef } from 'react';
+import './App.css'
+
+function App() {
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    
+    const number = formData.get('wasap')
+
+    const send = `https://api.whatsapp.com/send?phone=${number}`
+
+    window.location.replace(send)
+  }
+
+  return (
+    <div className='container'>
+      <h2>WhatsApp</h2>
+      <form action='submit' id='forum' onSubmit={handleSubmit} className=''>
+        <label htmlFor="wasap">Ingresá el número: </label>
+        <input type="number" name='wasap'id='wasap'/>
+        <button>Enter</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
